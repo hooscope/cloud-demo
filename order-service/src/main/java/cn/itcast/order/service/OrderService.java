50fe10fd73cc6e816937fc064fa6fc4e23d44812
hao wu <hooscope@gmail.com>
2022-07-12 21:40:52 +0800

    test_feature

--- order-service/src/main/java/cn/itcast/order/service/OrderService.java
+++ order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -7,10 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import javax.annotation.Resource;
+
 @Service
 public class OrderService {
 
-    @Autowired
+    @Resource
     private OrderMapper orderMapper;
     @Autowired
     private RestTemplate restTemplate;
@@ -18,14 +20,14 @@ public class OrderService {
     public Order queryOrderById(Long orderId) {
         // 1.查询订单
         Order order = orderMapper.findById(orderId);
-        // 2.利用RestTemplate发起http请求，查询用户
-        // 2.1.url路径
-//        String url = "http://userservice/user/" + order.getUserId();
-        // 2.2.发送http请求，实现远程调用
-//        User user = restTemplate.getForObject(url, User.class);
-        // 3.封装user到Order
-//        order.setUser(user);
-        // 4.返回
+//         2.利用RestTemplate发起http请求，查询用户
+//         2.1.url路径
+        String url = "http://userservice/user/" + order.getUserId();
+//         2.2.发送http请求，实现远程调用
+        User user = restTemplate.getForObject(url, User.class);
+//         3.封装user到Order
+        order.setUser(user);
+//         4.返回
         return order;
     }
 }
